087e1e9bcfa97ae603e2decd64c7d61c8d58bd58
swalke16 <swalke16@gmail.com>
2012-01-05 20:51:54 -0500

    added port switching for heroku

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 app.get('/', routes.introduction);
@@ -128,5 +128,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
