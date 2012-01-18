57c52db976f517d1fe99fd3ea432ad883ae7be95
Alex Burkhart <saterus@gmail.com>
2012-01-18 16:32:19 -0500

    DiscussionPanel reflects the app behavior now.

--- spec/discussion_panel_spec.js
+++ spec/discussion_panel_spec.js
@@ -1,9 +1,29 @@
 describe("DiscussionPanel", function(){
 
+  var $containerEl, discussionPanel;
+
+  beforeEach(function () {
+    $containerEl = $("<div id='discussion_pad'></div>");
+    discussionPanel = new views.DiscussionPanel($containerEl);
+  });
+
   it("is empty with 0 messages", function(){
-    var $containerEl = $("<div></div>");
-    var discussionPanel = new views.DiscussionPanel($containerEl);
     discussionPanel.render({messages:[]});
-    expect(discussionPanel.$el.children().length).toBe(0);
+    expect(discussionPanel.$discussion_items.children().length).toBe(0);
+  });
+
+  it("displays messages in chat", function(){
+    discussionPanel.render({messages:[{user:'alex', text:'first test text',  isForMe:false},
+                                      {user:'alex', text:'second test text', isForMe:false}]});
+    expect(discussionPanel.$discussion_items.children().length).toBe(2);
+
+  });
+
+  it("displays messages in chat", function(){
+    discussionPanel.render({messages:[{user:'alex', text:'this is for alex',  isForMe:true},
+                                      {user:'alex', text:'not for alex', isForMe:false}]});
+
+    expect(discussionPanel.$discussion_items.find('.forMe').length).toBe(1);
+
   });
 });
