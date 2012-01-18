f9e165b993dfb07446683bcb42cdae8819a5fbef
Alex Burkhart <saterus@gmail.com>
2012-01-18 14:20:42 -0500

    Added N user test.

--- spec/user_list_spec.js
+++ spec/user_list_spec.js
@@ -7,4 +7,11 @@ describe("UserList", function(){
     expect(userList.$el.children().length).toBe(0);
   });
 
+  it("should display N users with N users", function(){
+    var $containerEl = $("<div></div>");
+    var userList = new views.UserList($containerEl);
+    userList.render({users:[{name:'alex'}, {name:'kevin'}, {name:'scott'}]});
+    expect(userList.$el.children().length).toBe(3);
+  });
+
 });
