923c5b5cc9bb8329f4c10e49275d1921716da75d
Pairing Machine <pairing@gmail.com>
2012-01-19 13:01:35 -0500

    basic build brocess and watch script for handlebars templates

--- Jakefile.js
+++ Jakefile.js
@@ -1,17 +1,35 @@
-var exec = require("child_process").exec;
+var   fs = require("fs")
+    , exec = require("child_process").exec
+    , spawn = require("child_process").spawn
+    , glob = require("glob");
 
 desc("Watch files for changes and run downstream processes")
 task("watch", [], function(params){
+  fs.watch('./views/client/', function (event, filename) {
+    jake.Task['compile'].invoke();
+    jake.Task['compile'].reenable(true);
+  });
 
 });
 
 desc('Compile HANDLEBARS templates')
 task('compile', [], function(params) {
-  exec("handlebars -m -f ./public/javascripts/templates.js ./views/client/**/*.hbs", function(error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
+  glob("./views/client/**/*.hbs", {}, function(error, files){
+    if(error) throw error;
+
+    var compile = spawn("handlebars", ["-m", "-f", "./public/javascripts/templates.js"].concat(files));
+
+    compile.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+
+    compile.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+
+    compile.on('exit', function (code) {
+      console.log('compiling templates completed with code: ' + code);
+    });
+
   });
 });
